d8308ad82b37d4fc494f42d06628988b447fe078
anison99_ <anita.zych9941@gmail.com>
2022-12-01 13:14:27 +0100

    add time countdown and style home page

--- components/Home.js
+++ components/Home.js
@@ -4,12 +4,6 @@ import { View, Text, ScrollView, Animated, StyleSheet, Button } from 'react-nati
 
 import useAnimatedHeaderTitle from './useAnimatedTitle';
 
-import TestOne from './TestOne';
-import TestTwo from './TestTwo';
-import TestThree from './TestThree';
-
-
-
 
 export default function Home({ navigation }) {
 
@@ -23,26 +17,28 @@ export default function Home({ navigation }) {
     // -------
     return (
         <ScrollView onScroll={handleScroll} scrollEventThrottle={16}>
+
             <View style={{ flex: 1, height: 150}}>
                 <Text style={{ fontSize: 40, fontWeight: 'bold', marginLeft: 16, paddingTop: 30, color:'#3362AB' }}>Home Page</Text>
             </View>
-            
+
+
             <View style={styles.container}>
-                <Button title={"Test #1 \n \n lorem ipsum \n Press to take a test"} onPress={() => navigation.navigate("Test #1")} />
+                <Button color='#fff' title={"Test #1 \n \n lorem ipsum \n Press to take a test"} onPress={() => navigation.navigate("Test #1")} />
             </View>
 
             <View style={styles.container}>
-                <Button title={"Test #2 \n \n lorem ipsum \n Press to take a test"} onPress={() => navigation.navigate("Test #2")} />
+                <Button color='#fff' title={"Test #2 \n \n lorem ipsum \n Press to take a test"} onPress={() => navigation.navigate("Test #2")} />
             </View>
 
             <View style={styles.container}>
-                <Button title={"Test #3 \n \n lorem ipsum \n Press to take a test"} onPress={() => navigation.navigate("Test #3")} />
+                <Button color='#fff' title={"Test #3 \n \n lorem ipsum \n Press to take a test"} onPress={() => navigation.navigate("Test #3")} />
             </View>
 
             <View style={styles.resultPage}>
-                <Button color='#fff' title={"GET TO KNOW YOUR RESULTS!"} onPress={() => navigation.navigate("Results")}>
-                </Button>
+                <Button color='#fff' title={"GET TO KNOW YOUR RESULTS!"} onPress={() => navigation.navigate("Results")}> </Button>
             </View>
+            
 
         </ScrollView>
     );
@@ -52,9 +48,12 @@ const styles = StyleSheet.create({
     container: {
         height: 150,
         fontSize: 150,
-        backgroundColor: '#fff',
+        backgroundColor: '#608CCF',
         alignItems: 'center',
         justifyContent: 'center',
+        borderRadius: 25,
+
+
     },
     resultPage: {
         height: 150,
@@ -62,6 +61,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#3362AB',
         alignItems: 'center',
         justifyContent: 'center',
+        borderRadius: 25,
     },
 
 });
